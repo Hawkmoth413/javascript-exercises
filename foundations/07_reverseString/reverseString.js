fb71148ed0ba11df4d10e1754fee403a01a0f92e
hawkmoth413 <227488684+Hawkmoth413@users.noreply.github.com>
2026-08-21 17:55:16 -0400

    finish exercise 7 in foundations: 07_reversestring

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    str = str.split("");
+    let medianIndex = Math.floor(str.length / 2);
+    for(i = 0; i < medianIndex; i++) {
+        let temp = str[str.length-(i+1)]; //last elem is length-1.
+        str[str.length-(i+1)] = str[i];
+        str[i] = temp;
+    }
+    return str.join("");
 };
 
 // Do not edit below this line
