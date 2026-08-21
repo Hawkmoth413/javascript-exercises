86ea443490e5d56f09a499273468e1b0d65f546a
hawkmoth413 <227488684+Hawkmoth413@users.noreply.github.com>
2026-08-21 18:06:44 -0400

    finish exercise 8 of foundations: removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let args = [];
+    for(i = 0; i < arguments.length; i++) {
+        args[i] = arguments[i];
+    }
+    args.shift(); //as first argument is arr, we remove it
+
+    for(i = 0; i < args.length; i++) {
+        arr = arr.filter(item => item != args[i]);
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
