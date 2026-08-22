51a32381cc54559d60af6c56b6596450c2aa2216
hawkmoth413 <227488684+Hawkmoth413@users.noreply.github.com>
2026-08-22 15:54:01 -0400

    finish exercise 11 of foundations: tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let res = (tempF - 32) * (5/9);
+  return Math.round(res * 10) / 10; //convert to 1 decimal place too remember that
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let res = (tempC * 5/9 + 32);
+  return Math.round(res * 10) / 10; //also convert to 1 decimal place
 };
 
 // Do not edit below this line
