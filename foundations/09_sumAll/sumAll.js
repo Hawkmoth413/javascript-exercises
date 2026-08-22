3ef8c72eb221bb8297637ddf92e00981957a03fd
hawkmoth413 <227488684+Hawkmoth413@users.noreply.github.com>
2026-08-22 13:49:46 -0400

    finish exercise 9 of foundations: 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof(num1) != "number" || typeof(num2) != "number") {
+        return `ERROR`;
+    } 
+    else if(num1 <= 0 || num2 <= 0) {
+        return `ERROR`;
+    }
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
 
+    let res = 0;
+    for(i = start; i <= end; i++) {
+        res += i;
+    }
+    return res;
 };
 
 // Do not edit below this line
